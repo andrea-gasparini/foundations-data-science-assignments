fce5e9bc06545bde9b6229f341dcacd66b41bb6e
ædoardo <48515878+th3edo@users.noreply.github.com>
2020-10-29 22:22:13 +0100

    rpc_module done

--- homework1/Identification/rpc_module.py
+++ homework1/Identification/rpc_module.py
@@ -33,18 +33,19 @@ def plot_rpc(D, plot_color):
     
     tp = 0
     #... (your code here)
-    fp = 0
-    fn = sum(l)
+    
+    false_negative = num_images
+    false_positive = 0
     
     for idt in range(len(d)):
         tp += l[idt]
         #... (your code here)
-        fp = fp + (l[idt] == 0)
+        false_negative -= l[idt]
+        false_positive += (1 - l[idt])
         
-        #Compute precision and recall values and append them to "recall" and "precision" vectors
-        #... (your code here)
-        precision.append(tp/(tp/fp))
-        recall.append(tp/fn)
+        precision.append(tp/(tp+false_positive))
+        recall.append(tp/(tp+false_negative))
+        
     
     plt.plot([1-precision[i] for i in range(len(precision))], recall, plot_color+'-')
 
